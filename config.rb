4d0f5dba81984fa8a1a88b168a32b4cff146d9f6
Valentin Robert <vrobert@cs.ucsd.edu>
2015-06-11 14:51:07 -0700

    customized for Coq en stock

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/~vrobert/coq-en-stock/"
+http_images_path = "/~vrobert/coq-en-stock/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/~vrobert/coq-en-stock/fonts"
+css_dir = "public/~vrobert/coq-en-stock/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
